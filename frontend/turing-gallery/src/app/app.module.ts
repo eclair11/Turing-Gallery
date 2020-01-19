d9c26a95343a41660c9460ffb2d2ec0b1f6a0669
faresdabek <faresdabek@gmail.com>
2020-01-19 07:11:38 +0100

    first commit

--- frontend/turing-gallery/src/app/app.module.ts
+++ frontend/turing-gallery/src/app/app.module.ts
@@ -1,21 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser/';
+import { NgModule } from '@angular/core/';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestApiComponent } from './test-api/test-api.component';
-import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http/';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { HomeComponent } from './home/home.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestApiComponent
+    TestApiComponent,
+    InscriptionComponent,
+    HomeComponent,
+    ConnexionComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     HttpClientJsonpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

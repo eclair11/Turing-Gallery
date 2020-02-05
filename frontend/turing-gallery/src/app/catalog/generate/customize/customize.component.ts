8ab9d7b8950bf4ed1ad32bf7380c89e645e1292f
faresdabek <faresdabek@gmail.com>
2020-02-05 13:20:30 +0100

    modif home page + customize catalogue

--- frontend/turing-gallery/src/app/catalog/generate/customize/customize.component.ts
+++ frontend/turing-gallery/src/app/catalog/generate/customize/customize.component.ts
@@ -24,14 +24,8 @@ export class CustomizeComponent implements OnInit {
   pictures: Picture[] = [];
   pic1: Picture = null;
   pic2: Picture = null;
-  /** number of valid picture */
-  validPictureCount = 0;
-  /** number of invalid picture */
-  invalidPictureCount = 0;
-  /** delete picture indicator */
-  isShownDeletes = {};
-  shownDeleteIndex = -1;
-  deletedPicture = null;
+
+
 
 
   constructor(public fb: FormBuilder, private service: PictureService) {
@@ -99,87 +93,6 @@ export class CustomizeComponent implements OnInit {
         '';
   }
 
-
-  startUpload = () => {
-    this.service.post(this.pictures);
-    this.reinitPicture();
-  }
-
-  reinitPicture = () => {
-    this.pictures = [];
-    this.validPictureCount = 0;
-    this.invalidPictureCount = 0;
-  }
-
-  onClickDeletePicture = (picture: Picture): void => {
-    this.deletedPicture = picture;
-  }
-
-  deletePicture = (): void => {
-    if (this.deletedPicture !== null) {
-      this.pictures = this.pictures.filter((currentPicture) => {
-        return this.deletedPicture !== currentPicture;
-      });
-      this.updatePictureCounter();
-      this.reinitDeletedPicture();
-    }
-  }
-
-  reinitDeletedPicture = (): void => {
-    this.deletedPicture = null;
-  }
-
-  updatePictureCounter = (): void => {
-    if (this.deletedPicture.isValid) {
-      this.validPictureCount--;
-    } else {
-      this.invalidPictureCount--;
-    }
-  }
-
-  onMouseoverPicture = (index: number): void => {
-    for (let i = 0; i < this.pictures.length; i++) {
-      this.isShownDeletes['' + i] = false;
-    }
-    this.isShownDeletes['' + index] = true;
-    this.shownDeleteIndex = index;
-  }
-
-  onMouseleavePreview = (): void => {
-    this.isShownDeletes['' + this.shownDeleteIndex] = false;
-  }
-
-  preview = (files: any[]): void => {
-    console.log("prev1",files);
-    if (files.length === 0) {
-      return;
-    }
-    for (const file of files) {
-      // check if file is an image
-      if (file.type.match(/image\/*/) !== null) {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = (e) => {
-          const image = new Image();
-          image.src = reader.result as string;
-          image.onload = () => {
-            const picture = new Picture(
-              reader.result as string,
-              image.height,
-              image.width,
-              file
-            );
-            this.pictures.push(picture);
-            if (picture.isValid) {
-              this.validPictureCount++;
-            } else {
-              this.invalidPictureCount++;
-            }
-          };
-        };
-      }
-    }
-  }
 
   preview2 = (files: any[]): void => {
 

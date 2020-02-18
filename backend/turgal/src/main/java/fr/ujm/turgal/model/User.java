c2b7abfc99018992bf312cfed803e1dd03f70db6
faresdabek <faresdabek@gmail.com>
2020-02-18 15:11:23 +0100

    aa

--- backend/turgal/src/main/java/fr/ujm/turgal/model/User.java
+++ backend/turgal/src/main/java/fr/ujm/turgal/model/User.java
@@ -27,6 +27,8 @@ public class User {
     private String password;
     private String email;
 
+    
+
     @Temporal(TemporalType.DATE)
     private Date dateInscription = new Date();
 
@@ -44,6 +46,54 @@ public class User {
         this.displayname = displayname;
         this.password = password;
         this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDisplayname() {
+        return displayname;
+    }
+
+    public void setDisplayname(String displayname) {
+        this.displayname = displayname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateInscription() {
+        return dateInscription;
+    }
+
+    public void setDateInscription(Date dateInscription) {
+        this.dateInscription = dateInscription;
     }
 
 }
